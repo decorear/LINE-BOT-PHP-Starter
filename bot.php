0016d0352b72280c248f3d6488d91164b6e3de6e
decorear <comtoetoa@gmail.com>
2018-01-18 16:11:31 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -52,3 +52,7 @@ if (!is_null($events['events'])) {
 	}
 }
 echo "OK";
+$url = "https://api.bitfinex.com/v1/ticker/btcusd";
+$json = json_decode(file_get_contents($url), true);
+$price = $json["last_price"];
+echo $price;
