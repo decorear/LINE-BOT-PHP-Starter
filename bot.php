ddda0dd335c7c4d126af52ac9e70eea1e7b0ef66
decorear <comtoetoa@gmail.com>
2018-01-18 14:29:27 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,27 +1,47 @@
 <?php
+$access_token = 'oKQWl/XURFKCVIgvIUQ+Y/Kc8tFJxIWH8wlZp2B7vzBW0uBSb+Q+brh2gnzYuPzHPCvlWmqIbLK4BH1iv/1K11GPDzJ9U8nYVRVs69hdU33cOJDoRGHq+bcg4+1i2gDlVwsE135swWe4CEmXal3ylwdB04t89/1O/w1cDnyilFU=';
 
-// example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-01.php
-echo "I am a cryptobot";
-include ('line-bot-api/php/line-bot.php');
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
 
-$channelSecret = '6b134f677d24fc7fec476c02a78702c1';
-$access_token  = 'oKQWl/XURFKCVIgvIUQ+Y/Kc8tFJxIWH8wlZp2B7vzBW0uBSb+Q+brh2gnzYuPzHPCvlWmqIbLK4BH1iv/1K11GPDzJ9U8nYVRVs69hdU33cOJDoRGHq+bcg4+1i2gDlVwsE135swWe4CEmXal3ylwdB04t89/1O/w1cDnyilFU=
-';
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
 
-$bot = new BOT_API($channelSecret, $access_token);
-	
-if (!empty($bot->isEvents)) {
-		
-	$bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-	if ($bot->isSuccess()) {
-		echo 'Succeeded!';
-		exit();
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
 	}
-
-	// Failed
-	echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
-	exit();
-
 }
-?>
+echo "OK";
