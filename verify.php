7ebd0a2b5763f45f3aaa59876fb9e94665014a78
decorear <comtoetoa@gmail.com>
2018-01-18 14:51:30 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,7 +1,7 @@
 
 <?php
-$proxy = '54.173.229.200/24';
-$proxyauth = 'comtoetoa@gmail.com:idecorearX1*';
+//$proxy = '54.173.229.200/24';
+//$proxyauth = 'comtoetoa@gmail.com:pass*';
 
 $access_token = 'oKQWl/XURFKCVIgvIUQ+Y/Kc8tFJxIWH8wlZp2B7vzBW0uBSb+Q+brh2gnzYuPzHPCvlWmqIbLK4BH1iv/1K11GPDzJ9U8nYVRVs69hdU33cOJDoRGHq+bcg4+1i2gDlVwsE135swWe4CEmXal3ylwdB04t89/1O/w1cDnyilFU=';
 
@@ -14,8 +14,8 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 
-curl_setopt($ch, CURLOPT_PROXY, $proxy);
-curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+//curl_setopt($ch, CURLOPT_PROXY, $proxy);
+//curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 
 $result = curl_exec($ch);
 curl_close($ch);
